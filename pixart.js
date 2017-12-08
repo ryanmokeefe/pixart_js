dafc87453e76aacc2fe12688b2c92c7a2c34ad89
Ryan OKeefe <ryanokeefe208@gmail.com>
2017-12-07 19:57:09 -0500

    change brush color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,16 @@
+// Elements
 
+var button = document.getElementById('set-color')
+var brush = document.getElementsByClassName('brush')[0]
+var input = document.getElementById('color-field')
+
+// Function to change brush color: 
+
+var changeColor = function () {
+    event.preventDefault()
+    brush.style.backgroundColor = input.value;
+}
+
+// Event listener to call function
+
+button.addEventListener('click', changeColor)
