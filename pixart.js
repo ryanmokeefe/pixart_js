e53ae2c86dfaddb9c1dd72a080deba6f01b24e05
Ryan OKeefe <ryanokeefe208@gmail.com>
2017-12-07 20:37:45 -0500

    added 20 divs

--- pixart.js
+++ pixart.js
@@ -1,17 +1,20 @@
 // Elements
-
 var button = document.getElementById('set-color')
 var brush = document.getElementsByClassName('brush')[0]
 var input = document.getElementById('color-field')
 
 // Function to change brush color: 
-
 var changeColor = function () {
     event.preventDefault()
     brush.style.backgroundColor = input.value;
 }
 
 // Event listener to call function
-
-
 button.addEventListener('click', changeColor)
+
+// add Square class divs * 20
+for (i = 0; i < 20; i++) {
+    square = document.createElement('div');
+    square.className = "square";
+    document.body.appendChild(square);
+}
